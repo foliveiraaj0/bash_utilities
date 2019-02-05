907a872114a0255acbc9076f43b267d6a8de16af
Fernando Oliveira <f.oliveira.aj0@gmail.com>
2019-02-05 16:34:42 -0300

    handling filenames with spaces

--- legend_cleaner.sh
+++ legend_cleaner.sh
@@ -9,8 +9,8 @@ while read filename; do
 
   output=$(echo clean_$filename)
 
-  if [ -f $output ];then
-    rm $output
+  if [ -f "$output" ];then
+    rm "$output"
   fi
 
   cat "$filename" | sed '/^\([0-9]\)\{3\}/d' >> temp_legend
@@ -20,7 +20,7 @@ while read filename; do
   do 
     if [[ $line =~ ^[[:space:]]$ ]];then
       PREV_LINE+="\n"
-      echo -e $PREV_LINE >> $output
+      echo -e "$PREV_LINE" >> "$output"
       PREV_LINE=""
     else
       PREV_LINE+=$(echo $line | tr -d '\r' | tr -d '\n')
