607f4eef9ad99a71340399554fc2616d7078ba7c
Fernando Oliveira <f.oliveira.aj0@gmail.com>
2019-02-05 17:07:54 -0300

    running script in all valid files of the current folder

--- legend_cleaner.sh
+++ legend_cleaner.sh
@@ -4,37 +4,36 @@ if [ -f temp_legend ];then
   rm temp_legend
 fi
 
-find "$1" ! -name '*~' ! -name '*.sh' ! -name '\.' | sed 's/\.\///g' | 
-while read filename; do	
+for f in "."/*; do
 
-  output=$(echo clean_$filename)
+  find "$f" ! -name '*~' ! -name '*.sh' ! -name '\.' | sed 's/\.\///g' | 
+  while read filename; do	
 
-  if [ -f "$output" ];then
-    rm "$output"
-  fi
+    output=$(echo clean_$filename)
 
-  cat "$filename" | sed '/^\([0-9]\)\{3\}/d' >> temp_legend
-
-  cat temp_legend | 
-  while read line
-  do 
-    if [[ $line =~ ^[[:space:]]$ ]];then
-      PREV_LINE+="\n"
-      echo -e "$PREV_LINE" >> "$output"
-      PREV_LINE=""
-    else
-      PREV_LINE+=$(echo $line | tr -d '\r' | tr -d '\n')
-      if [[ $line =~ ,[0-9]{3} ]];then
-        PREV_LINE+=" : "
-      fi
+    if [ -f "$output" ];then
+      rm "$output"
     fi
+
+    cat "$filename" | sed '/^\([0-9]\)\{3\}/d' >> temp_legend
+
+    cat temp_legend | 
+      while read line
+	do 
+	  if [[ $line =~ ^[[:space:]]$ ]];then
+	    PREV_LINE+="\n"
+	    echo -e "$PREV_LINE" >> "$output"
+	      PREV_LINE=""
+	    else
+	      PREV_LINE+=$(echo $line | tr -d '\r' | tr -d '\n')
+	      if [[ $line =~ ,[0-9]{3} ]];then
+		PREV_LINE+=" : "
+	      fi
+	    fi
+
+      done	 
+    echo "$output"   
   done
-
   rm temp_legend
-	
 done
-
-
-
-
 
